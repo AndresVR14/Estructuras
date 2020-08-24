e2ebe8ceac85b6c2d4c6deb6f03ad0940e37f351
BatsySlayer <sa.fernandez@javeriana.edu.co>
2020-08-23 21:00:30 -0500

    Update Entrega_0_Grupo_1.cxx

--- Entrega_0_Grupo_1.cxx
+++ Entrega_0_Grupo_1.cxx
@@ -10,11 +10,15 @@
 */
 using namespace std;
 
+void help();
+
 int main(int argc, char *argv[]){
 	
 	cout<<"\t\t\t\t\t\tRISK++"<<endl;
 	char *comando = new char [20];
+	//cout<<strupr(comando)<<endl;
 	strcpy(comando, argv[1]);
+	help();
 	do{
 		cout<<endl<<"$"<<endl;
 		cout<<"Su comando fue: "<<comando<<endl;
@@ -24,4 +28,16 @@ int main(int argc, char *argv[]){
 	}while(strcmp(comando,"salir")!=0);
 
 	return 0;
+}
+
+void help(){//Esta función será guardada a la librería "help.h"
+	cout<<"\t\t\t\t\tHELP!"<<endl;
+	cout<<"Para obtener mas informacion acerca de un comando especifico, escriba HELP seguido del nombre de comando"<<endl;
+	cout<<"inicializar -> inicia el juego"<<endl;
+	cout<<"turno <id_jugador> -> inicia el turno del jugador indicado"<<endl;
+	cout<<"guardar <nombre_archivo> -> guarda el juego"<<endl;
+	cout<<"guardar_comprimido <nombre_archivo> -> guarda el juego en un archivo binario"<<endl;
+	cout<<"costo_conquista -> calcula el costo de invacion de territorios"<<endl;
+	cout<<"conquita_mas_barata -> calcula la conquista mas barata"<<endl;
+	cout<<"salir -> termina el programa"<<endl;
 }
