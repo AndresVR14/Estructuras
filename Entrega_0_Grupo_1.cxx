bc79eb9ad7ae34a356689fe5a9166dfc56a8b944
BatsySlayer <sa.fernandez@javeriana.edu.co>
2020-08-25 17:34:55 -0500

    Update Entrega_0_Grupo_1.cxx

--- Entrega_0_Grupo_1.cxx
+++ Entrega_0_Grupo_1.cxx
@@ -17,24 +17,41 @@ int main(int argc, char *argv[]){
 	
 	cout<<"\t\t\t\t\t\tRISK++"<<endl;
 	char *comando = new char [40];
-	char *coman2 = new char [40];
 	char *p = new char [40];
-	char *c = new char [40];
-	char *condicion = new char [40];
 	do{
+		char *coman2 = new char [40];
+		char *condicion = new char [40];
+		char *ayuda = new char [40];
 		do{
+			char *c = new char [40];
 			cout<<endl<<"$";
 			cin.getline(comando, 40);
 			strcpy(coman2, comando);
 			strcpy(p, comando);
 			coman2 = strtok(coman2, " ");
+			if(strcmp(comando, "help")==0){
+				ayuda = NULL;
+				break;
+			}else if(strcmp(coman2, "help")==0){
+				c = strtok(p, " ");
+				c = strtok(NULL, " ");
+				strcpy(ayuda, c);
+				if(!verificarComando(ayuda)){
+					break;
+				}
+			}
 			c = strtok(p, "<");
 			c = strtok(NULL, "<");
 			condicion=strtok(c, ">");
 			cout<<endl<<"Su comando fue: "<<comando<<endl;
+			delete c;
 		}while(!verificarComando(comando) && !verificarComando(coman2));
-		if(strcmp(comando, "help")==0){
-			help();
+		if(strcmp(coman2, "help")==0){
+			if(ayuda==NULL){
+				help();
+			}else{
+				
+			}
 		}else if(strcmp(comando,"inicializar")==0){
 		}else if(strcmp(comando,"costo_conquista")==0){
 		}else if(strcmp(comando,"conquita_mas_barata")==0){
@@ -42,6 +59,9 @@ int main(int argc, char *argv[]){
 		}else if(strcmp(coman2,"guardar")==0){
 		}else if(strcmp(coman2,"guardar_comprimido")==0){
 		}
+		delete coman2;
+		delete condicion;
+		delete ayuda;
 	}while(strcmp(comando,"salir")!=0);
 
 	return 0;
@@ -73,6 +93,8 @@ bool verificarComando(char *comando){
 	}else if(strcmp(comando,"costo_conquista")==0){
 		return true;
 	}else if(strcmp(comando,"conquista_mas_barata")==0){
+		return true;
+	}else if(strcmp(comando,"help")==0){
 		return true;
 	}
 	return false;
